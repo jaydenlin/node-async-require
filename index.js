eb40cb54d5efabffef445a725b95cdd8ccf99492
weiyu.lin <weiyu.in@rakuten.com>
2015-12-26 20:09:21 +0800

    add preParser config

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var requestSync = require('sync-request');
+var __ = require("lodash");
 
 
 var installed = false;
@@ -9,27 +10,42 @@ function install(options) {
     return;
   }
 
-  var preParser = options.preParser || "";
-  var queryString = options.queryString || "";
-
   require.extensions['.ajs'] = function(module, filename) {
 
+    ///////////////////////
+    ///Set up the preParser
+    ///////////////////////
+    var preParser = options.preParser || "";
+
+    //if the preParser is not a function, the use the buildt in preParsers
+    if (Object.prototype.toString.call(preParser) !== '[object Function]') {
+      switch (preParser) {
+        case "rt":
+          preParser = require("./preParser/reactTemplate.js");
+          break;
+        default:
+          preParser = function(rawContent) {
+            return rawContent;
+          };
+      }
+
+    }
+
+    //////////////////////////
+    ///Set up the queryString
+    //////////////////////////
+    var queryString = options.queryString || "";
+
+
+    ////////////////////////////
+    ///Fetch the remote contents
+    ////////////////////////////
     var nodeMoudleUrl = fs.readFileSync(filename, {
       encoding: 'utf8'
     });
     var res = requestSync('GET', nodeMoudleUrl);
     var rawContent = res.getBody();
-
-    switch (preParser) {
-      case "rt":
-        preParser = require("./preParser/reactTemplate.js");
-        break;
-      default:
-        preParser = function(rawContent) {
-          return rawContent;
-        };
-    }
-
+    //use preParser 
     var source = preParser(rawContent);
 
     module._compile(source, filename);
