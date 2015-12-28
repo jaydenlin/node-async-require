ee205e68ef6fc409f060731d2e084e177aa061e9
weiyu.lin <weiyu.in@rakuten.com>
2015-12-28 20:35:31 +0800

    change format of .ajs file to node module

--- index.js
+++ index.js
@@ -13,11 +13,30 @@ function install(options) {
   require.extensions['.ajs'] = function(module, filename) {
 
     ///////////////////////
+    ///Set up .ajs content
+    ///////////////////////
+    var content = fs.readFileSync(filename, {
+      encoding: 'utf8'
+    });
+
+    try {
+      content = eval(content);
+    } catch (e) {
+      throw new Error('The .ajs file is not a valid js. ' + e.toString());
+    }
+
+    if (Object.prototype.toString.call(content) !== '[object Object]') {
+      throw new Error('The .ajs file does not return a valid json');
+    } else if (typeof content.remoteUrl === "undefined") {
+      throw new Error('The .ajs file must return a remoteUrl');
+    }
+
+    ///////////////////////
     ///Set up the preParser
     ///////////////////////
     var preParser = options.preParser || "";
 
-    //if the preParser is not a function, the use the buildt in preParsers
+    //if the preParser is not a function, the use the buildt-in preParsers
     if (Object.prototype.toString.call(preParser) !== '[object Function]') {
       switch (preParser) {
         case "rt":
@@ -32,23 +51,48 @@ function install(options) {
     }
 
     //////////////////////////
+    ///Set up the async value
+    //////////////////////////
+    var async = options.async === false ? false : true;
+    if (async === false) {
+      if (typeof content.localPath === "undefined") {
+        throw new Error('The .ajs file must return a localPath, when you set async=false');
+      }
+    }
+
+
+
+    //////////////////////////
     ///Set up the queryString
     //////////////////////////
     var queryString = options.queryString || "";
 
 
-    ////////////////////////////
-    ///Fetch the remote contents
-    ////////////////////////////
-    var nodeMoudleUrl = fs.readFileSync(filename, {
-      encoding: 'utf8'
-    });
-    var res = requestSync('GET', nodeMoudleUrl);
-    var rawContent = res.getBody();
-    //use preParser 
-    var source = preParser(rawContent);
+    ////////////////////////////////////////////
+    ///Fetch the remote contents or local file
+    ///////////////////////////////////////////
+    if (async === true) {
+      //Fetch the remote contents
+      var nodeMoudleUrl = content.remoteUrl + queryString;
+      var res = requestSync('GET', nodeMoudleUrl);
+      var rawContent = res.getBody();
+      //use preParser 
+      var source = preParser(rawContent);
 
-    module._compile(source, filename);
+      module._compile(source, filename);
+    } else {
+      //Fetch loacl file
+      var rawContent = fs.readFileSync(content.localPath, {
+        encoding: 'utf8'
+      });
+      //use preParser 
+      var source = preParser(rawContent);
+      module._compile(source, filename);
+
+    }
+
+
+
   };
 
   installed = true;
